848f128bee871b92be912a5f6f13cfad8c5ad71f
MrDialloDev <salifdiallo@esp.sn>
2025-08-02 07:13:17 +0000

    Update file api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,14 +1,30 @@
 <?php
 
+use App\Http\Controllers\V1\AbsenceController;
+use App\Http\Controllers\V1\AnneeScolaireController;
+use App\Http\Controllers\V1\ClasseController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\V1\NoteController;
 use App\Http\Controllers\V1\ElevesController;
+use App\Http\Controllers\V1\EnseignementController;
+use App\Http\Controllers\V1\InscriptionController;
+use App\Http\Controllers\V1\MatiereController;
 use App\Http\Controllers\V1\MentionController;
 use App\Http\Controllers\V1\NiveauController;
+use App\Http\Controllers\V1\SemestreController;
+use App\Http\Controllers\V1\UeController;
 
 Route::prefix('v1')->group(function () {
     Route::apiResource('/notes', NoteController::class);
     Route::apiResource('/eleves', ElevesController::class);
     Route::apiResource('/niveaux', NiveauController::class);
     Route::apiResource('/mentions', MentionController::class);
+    Route::apiResource('/absences', AbsenceController::class);
+    Route::apiResource('/anneescolaires', AnneeScolaireController::class);
+    Route::apiResource('/classes', ClasseController::class);
+    Route::apiResource('/enseignements', EnseignementController::class);
+    Route::apiResource('/inscriptions', InscriptionController::class);
+    Route::apiResource('/matieres', MatiereController::class);
+    Route::apiResource('/semestres', SemestreController::class);
+    Route::apiResource('/ues', UeController::class);
 });
